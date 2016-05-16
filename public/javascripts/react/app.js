28aa2d3fdb44977da7aca2f646d979581a80e04c
Rahul Ravindran <rahulravindran2709@gmail.com>
2016-05-17 00:19:49 +0530

    working expense delete

--- public/javascripts/react/app.js
+++ public/javascripts/react/app.js
@@ -1,42 +1,95 @@
+"use strict";
+
 var ExpenseItem = React.createClass({
-	render :function(){
-		return (<div className="expense-item"><span className="expense-text">{this.props.expenseProp.name}</span><span className="expense-amount">${this.props.expenseProp.value}</span></div>);
+	displayName: "ExpenseItem",
+
+	deleteExpense: function deleteExpense() {
+		this.props.handleDelete(this.props.expenseProp.id);
+	},
+	render: function render() {
+		return React.createElement(
+			"div",
+			{ className: "expense-item " },
+			React.createElement(
+				"span",
+				{ className: "expense-text pull-left" },
+				this.props.expenseProp.name
+			),
+			React.createElement(
+				"span",
+				{ className: "expense-amount" },
+				"$",
+				this.props.expenseProp.value
+			),
+			React.createElement(
+				"a",
+				{ className: "remove-expense pull-right", onClick: this.deleteExpense },
+				"X"
+			)
+		);
 	}
 });
-
 
 var ExpenseTotal = React.createClass({
-	
-	render:function(){
-		return (<div className="expense-total">Grand Total:<span className="total-amount">${this.props.total}</span></div>);
-	}
+	displayName: "ExpenseTotal",
 
-	});
 
-var ExpenseApp = React.createClass({
-	handleGetExpenses:function(results){
-		this.setState({expenses:results})
-	},
-	getTotal:function(){
-		return  this.state.expenses.reduce(function(previousValue,currentValue){
-			return previousValue+currentValue.value;
-		},0);
-	},
-	getInitialState:function() {
-	    return {
-	        expenses:[] 
-	    };
-	},
-	componentDidMount:function() {
-	      $.get('/expenses',this.handleGetExpenses);
-	},
-	render:function(){
-		var expenseNodes = this.state.expenses.map(function(currentExpense){
-			return (<ExpenseItem key={currentExpense.id} expenseProp={currentExpense}/>);
-		});
-		return (<div className="expense-items">{expenseNodes}<hr /><ExpenseTotal total={this.getTotal()}/></div>);
+	render: function render() {
+		return React.createElement(
+			"div",
+			{ className: "expense-total" },
+			"Grand Total:",
+			React.createElement(
+				"span",
+				{ className: "total-amount" },
+				"$",
+				this.props.total
+			)
+		);
 	}
 
 });
 
-ReactDOM.render(<ExpenseApp />,document.getElementById('content'));
+var ExpenseApp = React.createClass({
+	displayName: "ExpenseApp",
+
+	removeItem: function removeItem(id) {
+		console.log('remove' + id);
+		this.state.expenses = this.state.expenses.filter(function (currentElem) {
+			return currentElem.id !== id;
+		});
+		this.setState({ expenses: this.state.expenses });
+	},
+	handleGetExpenses: function handleGetExpenses(results) {
+		this.setState({ expenses: results });
+	},
+	getTotal: function getTotal() {
+		return this.state.expenses.reduce(function (previousValue, currentValue) {
+			return previousValue + currentValue.value;
+		}, 0);
+	},
+	getInitialState: function getInitialState() {
+		return {
+			expenses: []
+		};
+	},
+	componentDidMount: function componentDidMount() {
+		$.get('/expenses', this.handleGetExpenses);
+	},
+	render: function render() {
+		var expenseAppComponent = this;
+		var expenseNodes = this.state.expenses.map(function (currentExpense) {
+			return React.createElement(ExpenseItem, { key: currentExpense.id, expenseProp: currentExpense, handleDelete: expenseAppComponent.removeItem });
+		});
+		return React.createElement(
+			"div",
+			{ className: "expense-items" },
+			expenseNodes,
+			React.createElement("hr", null),
+			React.createElement(ExpenseTotal, { total: this.getTotal() })
+		);
+	}
+
+});
+
+ReactDOM.render(React.createElement(ExpenseApp, null), document.getElementById('content'));
--- views/jsx/app.jsx
+++ views/jsx/app.jsx
@@ -1,13 +1,38 @@
 var ExpenseItem = React.createClass({
+	deleteExpense:function(){
+		this.props.handleDelete(this.props.expenseProp.id);
+	},
 	render :function(){
-		return (<div className="expense-item"><span className="expense-text">{this.props.expenseProp.name}</span><span className="expense-amount">${this.props.expenseProp.value}</span></div>);
+		return (<div className="expense-item "><span className="expense-text pull-left">{this.props.expenseProp.name}</span>
+			<span className="expense-amount">${this.props.expenseProp.value}</span>
+			<a className="remove-expense pull-right" onClick={this.deleteExpense}>X</a></div>);
 	}
 });
 
 
+var ExpenseTotal = React.createClass({
+	
+	render:function(){
+		return (<div className="expense-total">Grand Total:<span className="total-amount">${this.props.total}</span></div>);
+	}
+
+	});
+
 var ExpenseApp = React.createClass({
+	removeItem:function(id){
+		console.log('remove'+id);
+		this.state.expenses = this.state.expenses.filter(function(currentElem){
+			return currentElem.id!==id;
+		});
+		this.setState({expenses:this.state.expenses})
+	},
 	handleGetExpenses:function(results){
 		this.setState({expenses:results})
+	},
+	getTotal:function(){
+		return  this.state.expenses.reduce(function(previousValue,currentValue){
+			return previousValue+currentValue.value;
+		},0);
 	},
 	getInitialState:function() {
 	    return {
@@ -18,10 +43,11 @@ var ExpenseApp = React.createClass({
 	      $.get('/expenses',this.handleGetExpenses);
 	},
 	render:function(){
+		var expenseAppComponent = this;
 		var expenseNodes = this.state.expenses.map(function(currentExpense){
-			return (<ExpenseItem key={currentExpense.id} expenseProp={currentExpense}/>);
+			return (<ExpenseItem key={currentExpense.id} expenseProp={currentExpense} handleDelete={expenseAppComponent.removeItem}/>);
 		});
-		return (<div className="myclass">{expenseNodes}</div>);
+		return (<div className="expense-items">{expenseNodes}<hr /><ExpenseTotal total={this.getTotal()}/></div>);
 	}
 
 });
